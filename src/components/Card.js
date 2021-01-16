1134e6b300fdb5615fe5ff6f31ffcb9425fb5e96
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-01-15 22:45:52 -0500

    refactored so I wasn't repeating date computation code

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 
 import votedForTrump from '../data/electionResults';
+import { calculateImmunityDate } from '../data/computations';
 
 const useStyles = makeStyles({
   root: {
@@ -39,45 +40,7 @@ const useStyles = makeStyles({
 
 export default function OutlinedCard(props) {
   const classes = useStyles();
-
-  //https://www.w3resource.com/javascript-exercises/javascript-date-exercise-47.php
-  const diff_weeks = (dt2, dt1) => {
-    var diff =(dt2.getTime() - dt1.getTime()) / 1000;
-    diff /= (60 * 60 * 24 * 7);
-    return Math.abs(Math.round(diff));
-  }
-
-  const calculateImmunityDate = () => {
-
-      // we account for the 95% immunity rate of the vaccine by multiplying the num of currentVaccinated and weeklyDoses by .95
-      const currentVaccinated = Math.round(props.immunizationData['doses-number'] * .95);
-      const statePopulation = props.immunizationData.population;
-      let weeklyDoses = Math.round(props.immunizationData['v-diff-7'] * .95);
-
-      // to avoid dividing by 0, if no weekly doses were given (probably a data entry error) then we calculate avg weekly doses from dec. 21(week after first vaccine) til now
-      if(Number(weeklyDoses) === 0){
-          const firstVaccinationDate = new Date('December 21, 2020');
-          const now = new Date();
-          const num_weeks = diff_weeks(now, firstVaccinationDate);
-          weeklyDoses = currentVaccinated / num_weeks;
-      }
-
-      const population = Number(statePopulation.replace(/,/g, ""));
-
-      const herdImmunityNumber = population * .7;
-      const remaining = herdImmunityNumber - currentVaccinated;
-      const numWeeks = remaining / weeklyDoses;
-
-      const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-      let date = new Date();
-      date.setDate(date.getDate() + numWeeks * 7);
-      const month = months[date.getMonth()];
-      const day = date.getDay() + 1;
-      const year = date.getFullYear();
-      return month + " " + day + ", " + year + ".";
-  }
-
+  
   const renderBackgroundColor = (state) => {
     if(votedForTrump.includes(state)){
       return '#AA0000';
@@ -97,7 +60,7 @@ export default function OutlinedCard(props) {
             {props.content}
         </Typography>
         <Typography className={classes.info} variant="h5" component="h2">
-            {props.content} is expected to achieve herd immunity around {calculateImmunityDate()}
+            {props.content} is expected to achieve herd immunity around {calculateImmunityDate(props.immunizationData)}
         </Typography>
         <Grid container spacing={3} className={classes.statsContainer}>
             <Grid item xs={6} className={classes.stat}>
